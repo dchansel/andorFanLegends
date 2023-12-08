4294a99451b8a34a3607dd9a2f3fb3b271462875
dchansel <damien@13ruedebrioiux.com>
2023-12-08 11:56:18 +0100

    Filter options in own file

---
+++ src/options.js
@@ -0,0 +1,12 @@
+const yearOptions = ['2013','2014','2015','2016'];
+const boardOptions = [{name:'Andor',key:1},{name:'Cavern',key:2},{name:'Mer du Nord',key:3},{name:'Hadria',key:4},{name:'Montagnes grises',key:5},{name:'Krahd',key:6},{name:'Andor enneigé',key:7},{name:'Pays des trois frères',key:8}];
+const difficultiesOptions = [{name:'Facile', key:"1"}, {name:'Moyenne', key:"2"}, {name:'Difficile', key:"3"}];
+
+export default {
+    yearOptions: yearOptions,
+    boardOptions: boardOptions,
+    difficultiesOptions: difficultiesOptions
+    /*CARRIER: CARRIER,
+    COLLATION: COLLATION,
+    CASH_AND_CARRY: CASH_AND_CARRY*/
+  }
