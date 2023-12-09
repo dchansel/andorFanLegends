408e0e5b48478d639556a0fb59267d6c604374af
dchansel <damien@13ruedebrioiux.com>
2023-12-09 11:49:33 +0100

    Add locale ARG to the legend combine script + error if no locale

--- src/tools/combine.js
+++ src/tools/combine.js
@@ -1,16 +1,25 @@
-console.log("Go");
-console.log(__dirname);
+// Check if Locale
+if (!process.env.npm_config_locale) {
+    console.log('\x1b[31m' + "No Locale defined" + '\x1b[0m');
+    return
+}
+
+const locale = process.env.npm_config_locale;
+
+console.log("Start combine legends : " + locale);
 
 var fs = require('fs');
-var files = fs.readdirSync(__dirname + "../../../public/legends/fr/");
+var files = fs.readdirSync(__dirname + "../../../public/legends/" + locale + "/");
 
 //console.log(files);
 //console.log(files.length)
 legendList = [];
+count = 0;
 files.forEach( function (file) {
     if( file != "_legende-vierge.json" ) {
-
-        var obj = require(__dirname + "../../../public/legends/fr/" + file);
+        
+        var obj = require(__dirname + "../../../public/legends/" + locale + "/" + file);
+        console.log( obj.name);
 
         var shortLegend = {
             name: obj.name,
@@ -27,6 +36,8 @@ files.forEach( function (file) {
             download: obj.download
         };
         legendList.push(shortLegend);
+        console.log( shortLegend.name + " : ADDED");
+        count++;
     }
 });
 
@@ -34,10 +45,11 @@ const today = new Date();
 const yyyy = today.getFullYear();
 let mm = today.getMonth() + 1;
 let dd = today.getDate();
-fs.writeFile(__dirname + "../../../public/legends/legends-fr" + ".json", JSON.stringify(legendList), err => { //yyyy+mm+dd +
+fs.writeFile(__dirname + "../../../public/legends/legends-" + locale + ".json", JSON.stringify(legendList), err => { //yyyy+mm+dd +
     if (err) {
       console.error(err);
     }
 });
 
-console.log("DONE");
+console.log("FINISH combine legends");
+console.log("-> " + count + " legends have been summarize");
