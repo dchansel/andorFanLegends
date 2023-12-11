b82da5d4536a47c55e5f9ebe74362505c0920ef3
dchansel <damien@13ruedebrioiux.com>
2023-12-12 00:30:58 +0100

    add years in options

--- src/options.js
+++ src/options.js
@@ -1,4 +1,4 @@
-const yearOptions = ['2013','2014','2015','2016','2019'];
+const yearOptions = ['2013','2014','2015','2016','2017','2019','2020','2021'];
 const boardOptions = [{name:'Andor',key:"1"},{name:'Cavern',key:"2"},{name:'Mer du Nord',key:"3"},{name:'Hadria',key:"4"},{name:'Montagnes grises',key:"5"},{name:'Krahd',key:"6"},{name:'Andor enneigé',key:"7"},{name:'Pays des trois frères',key:"8"}];
 const difficultiesOptions = [{name:'Facile', key:"1"}, {name:'Moyenne', key:"2"}, {name:'Difficile', key:"3"}];
 
