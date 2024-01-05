e6e3dfceea8430457ebb81e0e03abd85f4589da4
dchansel <damien@13ruedebrioiux.com>
2024-01-05 13:52:43 +0100

    try fix analytics

--- src/firebase.js
+++ src/firebase.js
@@ -6,10 +6,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8TmqerEtrVgPNproerB-S1PfmYc08YJU",
+  /*apiKey: "AIzaSyD8TmqerEtrVgPNproerB-S1PfmYc08YJU",
   authDomain: "andorlegends.firebaseapp.com",
   projectId: "andorlegends",
   storageBucket: "andorlegends.appspot.com",
+  messagingSenderId: "79096540602",
+  appId: "1:79096540602:web:272a67d4c0aebb50a24183",
+  measurementId: "G-YG6NY08XCW"*/
+  apiKey: "AIzaSyD8TmqerEtrVgPNproerB-S1PfmYc08YJU",
+  authDomain: "andorlegends.web.app",
+  projectId: "andorfanlegends-e1bcf",
+  storageBucket: "andorfanlegends-e1bcf.appspot.com",
   messagingSenderId: "79096540602",
   appId: "1:79096540602:web:272a67d4c0aebb50a24183",
   measurementId: "G-YG6NY08XCW"
