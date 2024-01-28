9c92b28ee3eeb20017c5c6eb914a9a477dca4a02
dchansel <damien@13ruedebrioiux.com>
2024-01-28 08:17:07 +0100

    New firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  /*apiKey: "AIzaSyD8TmqerEtrVgPNproerB-S1PfmYc08YJU",
-  authDomain: "andorlegends.firebaseapp.com",
-  projectId: "andorlegends",
-  storageBucket: "andorlegends.appspot.com",
-  messagingSenderId: "79096540602",
-  appId: "1:79096540602:web:272a67d4c0aebb50a24183",
-  measurementId: "G-YG6NY08XCW"*/
+  /*
   apiKey: "AIzaSyD8TmqerEtrVgPNproerB-S1PfmYc08YJU",
   authDomain: "andorlegends.web.app",
   projectId: "andorfanlegends-e1bcf",
@@ -20,7 +14,17 @@ const firebaseConfig = {
   messagingSenderId: "79096540602",
   appId: "1:79096540602:web:272a67d4c0aebb50a24183",
   measurementId: "G-YG6NY08XCW"
+  */
+  apiKey: "AIzaSyCqD4G8n6du602PMz_zeozHahtV-wmKuJc",
+  authDomain: "andorfanlegends.web.app",
+  projectId: "andorfanlegends",
+  storageBucket: "andorfanlegends.appspot.com",
+  messagingSenderId: "871901879049",
+  appId: "1:871901879049:web:fcda711f3bfbb21b9b51d8",
+  measurementId: "G-TKNTKYE83J"
+  //measurementId: "G-YG6NY08XCW"
 };
+
 
 window.dataLayer = window.dataLayer || [];
 window.gtag = function(){window.dataLayer.push(arguments);}
